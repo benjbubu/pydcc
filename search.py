daece54214e0f339b57496e317aeb5f58ee2ed5b
benjbubu <benjbubu@barakuun.com>
2014-03-29 18:40:52 +0100

    Create search.py

---
+++ search.py
@@ -0,0 +1,13 @@
+#!/usr/lib/python
+
+#import urllib.request
+
+print "Pydcc"
+print " 1 : Rechercher"
+print " 2 : Voir le DL en cours"
+choix = input("Quel est votre choix?")
+
+if choix == 1:
+	print(choix)
+else:
+	print(choix)
