66b088d066223bc9502324b760b1b41bf293ea70
benjbubu <benjbubu@barakuun.com>
2014-03-29 19:58:09 +0100

    Update search.py
    Adding search

--- search.py
+++ search.py
@@ -1,13 +1,20 @@
 #!/usr/lib/python
 
-#import urllib.request
+import urllib2
 
+        
 print "Pydcc"
 print " 1 : Rechercher"
 print " 2 : Voir le DL en cours"
 choix = input("Quel est votre choix?")
 
 if choix == 1:
-	print(choix)
+        query = raw_input("Indiquez votre recherche \n")
+        url = "http://ixirc.com/?q=%s" % query.replace(" ","+")
+        recherche = urllib2.urlopen(url)
+        html = recherche.read()
+        print(url)
+        #print(html)
 else:
-	print(choix)
+        print(choix)
+
