29ab03df80fb5a73fff3a93abf5e380b7e282c4e
LoucIsZbib <massluc@gmail.com>
2014-03-30 14:07:54 +0200

    Update search.py
    parse(html) ajouté pour extraire les infos qui nous plaisent

--- search.py
+++ search.py
@@ -12,3 +12,24 @@ def search(requete, url=CONFIG.url):
         #html = recherche.read()
         return html
 
+def parse(html):
+        soup = BeautifulSoup(html)
+        tableResultat = soup.find(id="results-table")
+        nbreResultat = 0
+        resultats = []
+        for ligne in tableResultat.find_all(tr) :
+                for a in ligne.find_all(a):
+                        resultats[nbreResultats].append(a.string)
+                        # Resultats dans l'ordre 
+                        # 1-titre
+                        # 2-serveur
+                        # 3-channel
+                        # 4-user
+                        # 5- ?
+                        # 6- ?
+                        # 7-taille
+                        # 8- ?
+                        # 9- ?
+                nbreResultat += 1
+        return resultats
+        
