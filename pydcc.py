23ed189e5af861034d005a5cabae9ca1861ebcf3
LoucIsZbib <massluc@gmail.com>
2014-03-31 21:25:55 +0200

    ready for python 3

--- pydcc.py
+++ pydcc.py
@@ -7,17 +7,25 @@
 import config as CONFIG
 import search
         
-print "Pydcc"
-print " 1 : Rechercher"
-print " 2 : Voir le DL en cours"
-choix = input("Quel est votre choix?")
+print("Pydcc")
+print(" 1 : Rechercher")
+print(" 2 : Voir le DL en cours")
+try:
+        choix = input("Quel est votre choix?\n")
+        choix = int(choix)
+except:
+        print("erreur saisie/conversion choix")
 
-if choix == 1:
-        query = raw_input("Indiquez votre recherche \n")
-        html = search.search(query)
-        resultats = search.parse(html)
-        print(resultats)
-        # print(html)
+if choix == 1 :
+        try:
+                query = input("Indiquez votre recherche \n")
+        except:
+                print("erreur input recherche")
+        try:
+                resultats = search.searchAndParse(query)
+                print(resultats)
+        except:
+                print("erreur searchAndParse")
 else:
         print(choix)
 
--- search.py
+++ search.py
@@ -3,34 +3,50 @@
 # FUNCTIONS TO SEARCH AND PARSE RESULTS
 #
 
-import urllib2
+import urllib.request
 import config as CONFIG
+from bs4 import BeautifulSoup
         
-def search(requete, url=CONFIG.url):
-        url = "http://ixirc.com/?q=%s" % requete.replace(" ","+")
-        recherche = urllib2.urlopen(url).read()
-        #html = recherche.read()
-        return html
-
-def parse(html):
-        soup = BeautifulSoup(html)
-        tableResultat = soup.find(id="results-table")
-        nbreResultat = 0
-        resultats = []
-        for ligne in tableResultat.find_all(tr) :
-                resultats[nbreResultats] = []
-                for a in ligne.find_all(a):
-                        resultats[nbreResultats].append(a.string)
-                        # Resultats dans l'ordre 
-                        # 1-titre
-                        # 2-serveur
-                        # 3-channel
-                        # 4-user
-                        # 5- ?
-                        # 6- ?
-                        # 7-taille
-                        # 8- ?
-                        # 9- ?
-                nbreResultat += 1
+def searchAndParse(requete, url=CONFIG.url):
+        try:
+                url = "http://ixirc.com/?q=%s" % requete.replace(" ","+")
+                html = urllib.request.urlopen(url).read()
+        except:
+                print("searchAndPaste : erreur récupération page html")
+        
+        try:
+                soup = BeautifulSoup(html)
+        
+                # Version 1 : pas testé
+                #tableResultat = soup.find(id="results-table")
+                #table = BeautifulSoup(tableResultat)
+                #nbreResultat = 0
+                #resultats = []
+                #for ligne in tableResultat.find_all(tr) :
+                #        resultats[nbreResultats] = []
+                #        for a in ligne.find_all(a):
+                #                resultats[nbreResultats].append(a.string)
+                #                # Resultats dans l'ordre 
+                #                # 1-titre
+                #                # 2-serveur
+                #                # 3-channel
+                #                # 4-user
+                #                # 5- ?
+                #                # 6- ?
+                #                # 7-taille
+                #                # 8- ?
+                #                # 9- ?
+                #        nbreResultat += 1
+                
+                # Version 2
+                table = soup.findAll('a', {'class':'result-dl'})
+                resultats = []
+                for ligne in table :
+                    resultats.append(ligne.get('href'))
+                    print(ligne.get('href'))
+        except:
+                print("searchAndPaste : erreur parser")
+        
+        
         return resultats
         
