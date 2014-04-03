798db8c072175708614ad0a87ac3c5aa7fb5223e
LoucIsZbib <massluc@gmail.com>
2014-04-03 23:27:07 +0200

    IRC bot pour python 3 - se connecte a 1 ou plusieurs channel

--- bot.py
+++ bot.py
@@ -1,15 +1,79 @@
 #!/usr/bin/python
+# -*- coding: utf8 -*-
 #Python2.X needed + IrcLib/Bot
-import irclib
-import ircbot
-import config as CONFIG
+import irc.bot
+import irc.strings
+from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
+#import config as CONFIG
 
-class Grabator(ircbot.SingleServerIRCBot):
-    def __init__(self):
-        ircbot.SingleServerIRCBot.__init__(self, [(CONFIG.ircServerName,CONFIG.ircPort)], CONFIG.ircNickname, CONFIG.ircUserName)
+class Grabator(irc.bot.SingleServerIRCBot):
+    def __init__(self, channel="#barakuun", nickname="Pydcc", server="irc.kottnet.net", port=6667):
+        print(channel, nickname, server, port) #DEBUG
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        self.channel = channel
     
-    def on_welcome(self, serv, ev):
-	serv.join(CONFIG.ircChannel)
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
 
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+        # DEBUG : se connecter à un autre channel en parallèle :)
+        c.join("#barakuun2")
+
+    def on_privmsg(self, c, e):
+        self.do_command(e, e.arguments[0])
+        
+    def on_pubmsg(self, c, e):
+        a = e.arguments[0].split(":", 1)
+        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
+            self.do_command(e, a[1].strip())
+        return
+
+    def on_dccmsg(self, c, e):
+        c.privmsg("You said: " + e.arguments[0])
+
+    def on_dccchat(self, c, e):
+        if len(e.arguments) != 2:
+            return
+        args = e.arguments[1].split()
+        if len(args) == 4:
+            try:
+                address = ip_numstr_to_quad(args[2])
+                port = int(args[3])
+            except ValueError:
+                return
+            self.dcc_connect(address, port)
+
+    def do_command(self, e, cmd):
+        nick = e.source.nick
+        c = self.connection
+
+        if cmd == "disconnect":
+            self.disconnect()
+        elif cmd == "die":
+            self.die()
+        elif cmd == "stats":
+            for chname, chobj in self.channels.items():
+                c.notice(nick, "--- Channel statistics ---")
+                c.notice(nick, "Channel: " + chname)
+                users = chobj.users()
+                users.sort()
+                c.notice(nick, "Users: " + ", ".join(users))
+                opers = chobj.opers()
+                opers.sort()
+                c.notice(nick, "Opers: " + ", ".join(opers))
+                voiced = chobj.voiced()
+                voiced.sort()
+                c.notice(nick, "Voiced: " + ", ".join(voiced))
+        elif cmd == "dcc":
+            dcc = self.dcc_listen()
+            c.ctcp("DCC", nick, "CHAT chat %s %d" % (
+                ip_quad_to_numstr(dcc.localaddress),
+                dcc.localport))
+        else:
+            c.notice(nick, "Not understood: " + cmd)
+            
 if __name__ == "__main__":
-    Grabator().start()
+    print("bot.py - fonction test")
+    bot = Grabator()
+    bot.start()
