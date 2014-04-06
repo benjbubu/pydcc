e6dce241bc5435a37c5e8409a8c0d20387e02b67
LoucIsZbib <massluc@gmail.com>
2014-04-06 17:56:10 +0200

    connection et dl ok sur 3 serveurs a faire : - selection auto du channel de "discussion"

--- bot.py
+++ bot.py
@@ -16,106 +16,98 @@ import irc.strings
 #from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
 #import config as CONFIG
 
+import time
 
 class Grabator(irc.bot.SingleServerIRCBot):
-    def __init__(self, channel="#barakuun", nickname="Pydcc", server="irc.kottnet.net", port=6667):
-        print(channel, nickname, server, port) #DEBUG
-        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+    def __init__(
+            self, 
+            channel="#barakuun", 
+            nickname="Pydcc", 
+            server="irc.kottnet.net", 
+            numPaquet=0, 
+            nomRobot="Paul", 
+            secondChannel="#barakuun2",
+            port=6667,):
+        print("channel =", channel, "; nickname =", nickname, "; server = ", server, port) #DEBUG
+        try:
+            irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        except:
+            print("bot.py : pb création bot/connexion server")
         self.channel = channel
+        self.numPaquet = numPaquet
+        self.nomRobot = nomRobot
+        self.secondChannel = secondChannel
         self.received_bytes = 0
+        self.ctcp_version = "HexChat 2.9.5"
     
-    def on_nicknameinuse(self, c, e):
+    def on_nicknameinuse(self, c, event):
         c.nick(c.get_nickname() + "_")
 
-    def on_welcome(self, c, e):
+    def on_welcome(self, c, event):
         c.join(self.channel)
         self.connection.buffer.errors = 'replace'
-        # DEBUG : se connecter à un autre channel en parallèle :)
-        #c.join("#barakuun2")
-
-    def on_privmsg(self, c, e):
-        pass
-        #self.do_command(e, e.arguments[0])
         
-    def on_pubmsg(self, c, e):
+        c.join(self.secondChannel)  
+        
+        time.sleep(5.2)
+        c.notice(self.nomRobot, "xdcc send #" + str(self.numPaquet) )
+        
+
+    def on_privmsg(self, connection, event):
+        #DEBUG
+        print(event.arguments)
+        
+    def on_pubmsg(self, cconnection, event):
         pass
-        # a = e.arguments[0].split(":", 1)
-        # if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
-            # self.do_command(e, a[1].strip())
-        # return
 
     def on_dccmsg(self, connection, event):
         #c.privmsg("You said: " + e.arguments[0])
+        
         data = event.arguments[0]
         self.file.write(data)
         self.received_bytes = self.received_bytes + len(data)
         self.dcc.send_bytes(struct.pack("!I", self.received_bytes))
 
-    def on_dccchat(self, c, e):
+    def on_dccchat(self, connection, event):
         pass
-        # if len(e.arguments) != 2:
-            # return
-        # args = e.arguments[1].split()
-        # if len(args) == 4:
-            # try:
-                # address = ip_numstr_to_quad(args[2])
-                # port = int(args[3])
-            # except ValueError:
-                # return
-            # self.dcc_connect(address, port)
 
-    def do_command(self, e, cmd):
+
+    def do_command(self, event, cmd):
         pass
-        #nick = e.source.nick
-        # c = self.connection
 
-        # if cmd == "disconnect":
-            # self.disconnect()
-        # elif cmd == "die":
-            # self.die()
-        # elif cmd == "stats":
-            # for chname, chobj in self.channels.items():
-                # c.notice(nick, "--- Channel statistics ---")
-                # c.notice(nick, "Channel: " + chname)
-                # users = chobj.users()
-                # users.sort()
-                # c.notice(nick, "Users: " + ", ".join(users))
-                # opers = chobj.opers()
-                # opers.sort()
-                # c.notice(nick, "Opers: " + ", ".join(opers))
-                # voiced = chobj.voiced()
-                # voiced.sort()
-                # c.notice(nick, "Voiced: " + ", ".join(voiced))
-        # elif cmd == "dcc":
-            # dcc = self.dcc_listen()
-            # c.ctcp("DCC", nick, "CHAT chat %s %d" % (
-                # ip_quad_to_numstr(dcc.localaddress),
-                # dcc.localport))
-        # elif cmd == "transferator":
-            # c.notice(nick, "je vais démarrer le transfert avec transferator")
-            # c.notice("Transferator", "xdcc send #11")
-        # else:
-            # c.notice(nick, "Not understood: " + cmd)
-         
     def on_ctcp(self, connection, event):
-        args = event.arguments[1].split()
-        if args[0] != "SEND":
-            return
-        self.filename = os.path.basename(args[1])
-        if os.path.exists(self.filename):
-            print("A file named", self.filename,)
-            print("already exists. Refusing to save it.")
-            self.connection.quit()
-        self.file = open(self.filename, "wb")
-        peeraddress = irc.client.ip_numstr_to_quad(args[2])
-        peerport = int(args[3])
-        self.dcc = self.dcc_connect(peeraddress, peerport, "raw")
+    
+        #DEBUG
+        print(event.arguments)
+        
+        # le handler par défaut
+        nick = event.source.nick
+        if event.arguments[0] == "VERSION":
+            connection.ctcp_reply(nick, "VERSION " + self.ctcp_version)
+        elif event.arguments[0] == "PING":
+            if len(event.arguments) > 1:
+                connection.ctcp_reply(nick, "PING " + event.arguments[1])
+        elif len(event.arguments) >= 2:       
+            args = event.arguments[1].split()
+            if args[0] != "SEND":
+                print("bot.py : error : dcc command incomprise")
+                return
+            elif args[0] == "SEND":
+                self.filename = os.path.basename(args[1])
+                if os.path.exists(self.filename):
+                    print("A file named", self.filename,)
+                    print("already exists. Refusing to save it.")
+                    self.connection.quit()
+                self.file = open(self.filename, "wb")
+                peeraddress = irc.client.ip_numstr_to_quad(args[2])
+                peerport = int(args[3])
+                self.dcc = self.dcc_connect(peeraddress, peerport, "raw")
           
     def on_dcc_disconnect(self, connection, event):
         self.file.close()
         print("Received file %s (%d bytes)." % (self.filename,
                                                 self.received_bytes))
-        #self.connection.quit()
+        self.connection.quit()
 
 
 if __name__ == "__main__":
